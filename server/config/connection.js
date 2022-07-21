029e01c7b00e5be6216e394455688ec973bf29f6
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-21 15:13:26 +1000

    Added dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
